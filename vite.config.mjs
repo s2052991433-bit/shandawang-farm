4f1f90196c50693da75ac579c440ca5ceb6b4a84
s2052991433-bit <s2052991433@gmail.com>
2026-08-24 02:27:09 +0800

    Configure custom domain asset paths

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/shandawang-farm/",
+  base: "/",
   build: {
     outDir: "dist/client",
   },
